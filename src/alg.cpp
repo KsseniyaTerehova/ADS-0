ff4c7a2dca3421892e8b720a4aa4a6a4240a15c0
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-02-25 19:22:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a == 0 || b == 0)
+        return a + b;
+    if (a > b)
+        return gcd(a - b, b);
+    else return gcd(a, b - a);
 }
